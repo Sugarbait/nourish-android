27beaa2f15b73f2764793dde75578cf7c704b107
Mike Moore <clientele@Mikes-MacBook-Pro.local>
2026-04-16 16:21:38 -0400

    Update build version in menu to 0.2.0

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-const BUILD_VERSION = "1.5.27";
+const BUILD_VERSION = "0.2.0";
 
 import React, { useState, useMemo, useRef, useEffect, useCallback } from 'react';
 import Image from 'next/image';
